e767322066b8c48af1a81b37421eeb5162c31d4b
Thomas S. Benjamin <tomb@disruptdev.com>
2015-07-07 19:25:36 -0400

    functional test intended to test compatability of HasOneOrMany::saveMany() with newer versions of Eloquent

--- tests/functional/HasManyRelationTest.php
+++ tests/functional/HasManyRelationTest.php
@@ -1,4 +1,6 @@
-<?php namespace Vinelab\NeoEloquent\Tests\Functional\Relations\HasMany;
+<?php
+
+namespace Vinelab\NeoEloquent\Tests\Functional\Relations\HasMany;
 
 use Mockery as M;
 use Vinelab\NeoEloquent\Tests\TestCase;
@@ -7,33 +9,38 @@ use Vinelab\NeoEloquent\Eloquent\Model;
 class Book extends Model {
 
     protected $label = 'Book';
-
     protected $fillable = ['title', 'pages', 'release_date'];
+
+    // I (tomb10516) added this relation because some functionality was being untested that can
+    // now be tested with this relation
+    public function author() {
+        return $this->belongsTo('Vinelab\NeoEloquent\Tests\Functional\Relations\HasMany\Author', 'WROTE');
+    }
+
 }
 
 class Author extends Model {
 
     protected $label = 'Author';
-
     protected $fillable = ['name'];
 
-    public function books()
-    {
+    public function books() {
         return $this->hasMany('Vinelab\NeoEloquent\Tests\Functional\Relations\HasMany\Book', 'WROTE');
     }
+
 }
 
 class HasManyRelationTest extends TestCase {
 
-    public function tearDown()
-    {
-        M::close();
+//
+//    public function tearDown()
+//    {
+//        M::close();
+//
+//        parent::tearDown();
+//    }
 
-        parent::tearDown();
-    }
-
-    public function setUp()
-    {
+    public function setUp() {
         parent::setUp();
 
         $resolver = M::mock('Illuminate\Database\ConnectionResolverInterface');
@@ -43,8 +50,7 @@ class HasManyRelationTest extends TestCase {
         Book::setConnectionResolver($resolver);
     }
 
-    public function testSavingSingleAndDynamicLoading()
-    {
+    public function testSavingSingleAndDynamicLoading() {
         $author = Author::create(['name' => 'George R. R. Martin']);
         $got = new Book(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
         $cok = new Book(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
@@ -75,31 +81,30 @@ class HasManyRelationTest extends TestCase {
         $writtenCok->delete();
     }
 
-    public function testSavingManyAndDynamicLoading()
-    {
+    public function testSavingManyAndDynamicLoading() {
         $author = Author::create(['name' => 'George R. R. Martin']);
 
         $novel = [
             new Book([
-                'title'        => 'A Game of Thrones',
-                'pages'        => 704,
+                'title' => 'A Game of Thrones',
+                'pages' => 704,
                 'release_date' => 'August 1996'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Clash of Kings',
-                'pages'        => 768,
+                'title' => 'A Clash of Kings',
+                'pages' => 768,
                 'release_date' => 'February 1999'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Storm of Swords',
-                'pages'        => 992,
+                'title' => 'A Storm of Swords',
+                'pages' => 992,
                 'release_date' => 'November 2000'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Feast for Crows',
-                'pages'        => 753,
+                'title' => 'A Feast for Crows',
+                'pages' => 753,
                 'release_date' => 'November 2005'
-            ])
+                    ])
         ];
 
         $edges = $author->books()->saveMany($novel);
@@ -108,8 +113,7 @@ class HasManyRelationTest extends TestCase {
         $books = $author->books->toArray();
         $this->assertCount(count($novel), $books);
 
-        foreach ($edges as $key => $edge)
-        {
+        foreach ($edges as $key => $edge) {
             $this->assertInstanceOf('Vinelab\NeoEloquent\Eloquent\Edges\EdgeOut', $edge);
             $this->assertTrue($edge->exists());
             $this->assertGreaterThan(0, $edge->id);
@@ -119,35 +123,33 @@ class HasManyRelationTest extends TestCase {
         }
     }
 
-    public function testCreatingSingleRelatedModels()
-    {
+    public function testCreatingSingleRelatedModels() {
         $author = Author::create(['name' => 'George R. R. Martin']);
 
         $novel = [
             [
-                'title'        => 'A Game of Thrones',
-                'pages'        => 704,
+                'title' => 'A Game of Thrones',
+                'pages' => 704,
                 'release_date' => 'August 1996'
             ],
             [
-                'title'        => 'A Clash of Kings',
-                'pages'        => 768,
+                'title' => 'A Clash of Kings',
+                'pages' => 768,
                 'release_date' => 'February 1999'
             ],
             [
-                'title'        => 'A Storm of Swords',
-                'pages'        => 992,
+                'title' => 'A Storm of Swords',
+                'pages' => 992,
                 'release_date' => 'November 2000'
             ],
             [
-                'title'        => 'A Feast for Crows',
-                'pages'        => 753,
+                'title' => 'A Feast for Crows',
+                'pages' => 753,
                 'release_date' => 'November 2005'
             ]
         ];
 
-        foreach ($novel as $book)
-        {
+        foreach ($novel as $book) {
             $edge = $author->books()->create($book, ['on' => $book['release_date']]);
 
             $this->assertInstanceOf('Vinelab\NeoEloquent\Eloquent\Edges\EdgeOut', $edge);
@@ -160,37 +162,89 @@ class HasManyRelationTest extends TestCase {
         }
     }
 
-        public function testCreatingManyRelatedModels()
-    {
+    public function testCreatingSingleBilateralRelatedModels() {
         $author = Author::create(['name' => 'George R. R. Martin']);
 
         $novel = [
             [
-                'title'        => 'A Game of Thrones',
-                'pages'        => 704,
+                'title' => 'A Game of Thrones',
+                'pages' => 704,
                 'release_date' => 'August 1996'
             ],
             [
-                'title'        => 'A Clash of Kings',
-                'pages'        => 768,
+                'title' => 'A Clash of Kings',
+                'pages' => 768,
                 'release_date' => 'February 1999'
             ],
             [
-                'title'        => 'A Storm of Swords',
-                'pages'        => 992,
+                'title' => 'A Storm of Swords',
+                'pages' => 992,
                 'release_date' => 'November 2000'
             ],
             [
-                'title'        => 'A Feast for Crows',
-                'pages'        => 753,
+                'title' => 'A Feast for Crows',
+                'pages' => 753,
+                'release_date' => 'November 2005'
+            ]
+        ];
+
+        foreach ($novel as $book) {
+            $edge = $author->books()->create($book, ['on' => $book['release_date']]);
+
+
+            $this->assertInstanceOf('Vinelab\NeoEloquent\Eloquent\Edges\EdgeOut', $edge);
+            $this->assertTrue($edge->exists());
+            $this->assertGreaterThan(0, $edge->id);
+            $this->assertNotNull($edge->created_at);
+            $this->assertNotNull($edge->updated_at);
+            $this->assertEquals($edge->on, $book['release_date']);
+
+            print_r("\n" . $edge->id . "\n");
+
+
+            $bookModel = $edge->related();
+            // this following line throws an exception if issue #104 is not fixed
+            $bookModel->author()->associate($author)->save();
+
+            print_r($edge->id . "\n");
+
+            try {
+            $edge->delete();
+            } catch (\Exception $e) {
+                dd($edge);
+            }
+        }
+    }
+
+    public function testCreatingManyRelatedModels() {
+        $author = Author::create(['name' => 'George R. R. Martin']);
+
+        $novel = [
+            [
+                'title' => 'A Game of Thrones',
+                'pages' => 704,
+                'release_date' => 'August 1996'
+            ],
+            [
+                'title' => 'A Clash of Kings',
+                'pages' => 768,
+                'release_date' => 'February 1999'
+            ],
+            [
+                'title' => 'A Storm of Swords',
+                'pages' => 992,
+                'release_date' => 'November 2000'
+            ],
+            [
+                'title' => 'A Feast for Crows',
+                'pages' => 753,
                 'release_date' => 'November 2005'
             ]
         ];
 
         $edges = $author->books()->createMany($novel);
 
-        foreach ($edges as $edge)
-        {
+        foreach ($edges as $edge) {
             $this->assertInstanceOf('Vinelab\NeoEloquent\Eloquent\Edges\EdgeOut', $edge);
             $this->assertTrue($edge->exists());
             $this->assertGreaterThan(0, $edge->id);
@@ -201,31 +255,30 @@ class HasManyRelationTest extends TestCase {
         }
     }
 
-    public function testEagerLoadingHasMany()
-    {
+    public function testEagerLoadingHasMany() {
         $author = Author::create(['name' => 'George R. R. Martin']);
 
         $novel = [
             new Book([
-                'title'        => 'A Game of Thrones',
-                'pages'        => 704,
+                'title' => 'A Game of Thrones',
+                'pages' => 704,
                 'release_date' => 'August 1996'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Clash of Kings',
-                'pages'        => 768,
+                'title' => 'A Clash of Kings',
+                'pages' => 768,
                 'release_date' => 'February 1999'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Storm of Swords',
-                'pages'        => 992,
+                'title' => 'A Storm of Swords',
+                'pages' => 992,
                 'release_date' => 'November 2000'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Feast for Crows',
-                'pages'        => 753,
+                'title' => 'A Feast for Crows',
+                'pages' => 753,
                 'release_date' => 'November 2005'
-            ])
+                    ])
         ];
 
         $edges = $author->books()->saveMany($novel);
@@ -237,57 +290,53 @@ class HasManyRelationTest extends TestCase {
         $this->assertArrayHasKey('books', $relations);
         $this->assertCount(count($novel), $relations['books']->toArray());
 
-        foreach ($relations['books'] as $key => $book)
-        {
+        foreach ($relations['books'] as $key => $book) {
             $this->assertEquals($novel[$key]->toArray(), $book->toArray());
             $edge = $author->books()->edge($book);
             $this->assertTrue($edge->delete());
         }
     }
 
-    public function testSavingManyRelationsWithRelationProperties()
-    {
+    public function testSavingManyRelationsWithRelationProperties() {
         $author = Author::create(['name' => 'George R. R. Martin']);
 
         $novel = [
             new Book([
-                'title'        => 'A Game of Thrones',
-                'pages'        => 704,
+                'title' => 'A Game of Thrones',
+                'pages' => 704,
                 'release_date' => 'August 1996'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Clash of Kings',
-                'pages'        => 768,
+                'title' => 'A Clash of Kings',
+                'pages' => 768,
                 'release_date' => 'February 1999'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Storm of Swords',
-                'pages'        => 992,
+                'title' => 'A Storm of Swords',
+                'pages' => 992,
                 'release_date' => 'November 2000'
-            ]),
+                    ]),
             new Book([
-                'title'        => 'A Feast for Crows',
-                'pages'        => 753,
+                'title' => 'A Feast for Crows',
+                'pages' => 753,
                 'release_date' => 'November 2005'
-            ])
+                    ])
         ];
 
         $edges = $author->books()->saveMany($novel, ['novel' => true]);
         $this->assertCount(count($novel), $edges->toArray());
 
-        foreach ($edges as $edge)
-        {
+        foreach ($edges as $edge) {
             $this->assertTrue($edge->novel);
             $edge->delete();
         }
     }
 
-    public function testSyncingModelIds()
-    {
+    public function testSyncingModelIds() {
         $author = Author::create(['name' => 'George R.R. Martin']);
-        $bk     = Book::create(['title' => 'foo']);
-        $got    = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
-        $cok    = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
+        $bk = Book::create(['title' => 'foo']);
+        $got = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
+        $cok = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
 
         $author->books()->attach($bk);
 
@@ -295,19 +344,20 @@ class HasManyRelationTest extends TestCase {
 
         $edges = $author->books()->edges();
 
-        $edgesIds = array_map(function($edge) { return $edge->getRelated()->getKey(); }, $edges->toArray());
+        $edgesIds = array_map(function($edge) {
+            return $edge->getRelated()->getKey();
+        }, $edges->toArray());
 
         $this->assertTrue(in_array($got->id, $edgesIds));
         $this->assertTrue(in_array($cok->id, $edgesIds));
         $this->assertFalse(in_array($bk->id, $edgesIds));
     }
 
-    public function testSyncingWithIdsUpdatesModels()
-    {
+    public function testSyncingWithIdsUpdatesModels() {
         $author = Author::create(['name' => 'George R.R. Martin']);
-        $got    = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
-        $cok    = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
-        $sos    = Book::create(['title' => 'A Storm of Swords', 'pages' => 992, 'release_date' => 'November 2000']);
+        $got = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
+        $cok = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
+        $sos = Book::create(['title' => 'A Storm of Swords', 'pages' => 992, 'release_date' => 'November 2000']);
 
         $author->books()->attach($got);
 
@@ -315,19 +365,20 @@ class HasManyRelationTest extends TestCase {
 
         $edges = $author->books()->edges();
 
-        $edgesIds = array_map(function($edge) { return $edge->getRelated()->getKey(); }, $edges->toArray());
+        $edgesIds = array_map(function($edge) {
+            return $edge->getRelated()->getKey();
+        }, $edges->toArray());
 
         $this->assertTrue(in_array($got->id, $edgesIds));
         $this->assertTrue(in_array($cok->id, $edgesIds));
         $this->assertTrue(in_array($sos->id, $edgesIds));
     }
 
-    public function testSyncingWithAttributes()
-    {
+    public function testSyncingWithAttributes() {
         $author = Author::create(['name' => 'George R.R. Martin']);
-        $got    = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
-        $cok    = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
-        $sos    = Book::create(['title' => 'A Storm of Swords', 'pages' => 992, 'release_date' => 'November 2000']);
+        $got = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
+        $cok = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
+        $sos = Book::create(['title' => 'A Storm of Swords', 'pages' => 992, 'release_date' => 'November 2000']);
 
         $author->books()->attach($got);
 
@@ -339,7 +390,9 @@ class HasManyRelationTest extends TestCase {
 
         $edges = $author->books()->edges();
 
-        $edgesIds = array_map(function($edge) { return $edge->getRelated()->getKey(); }, $edges->toArray());
+        $edgesIds = array_map(function($edge) {
+            return $edge->getRelated()->getKey();
+        }, $edges->toArray());
 
         $count = array_count_values((array) $got->id);
 
@@ -350,8 +403,7 @@ class HasManyRelationTest extends TestCase {
 
         $expectedEdgesTypes = array('Game', 'Clash', 'Storm');
 
-        foreach ($edges as $key => $edge)
-        {
+        foreach ($edges as $key => $edge) {
             $attributes = $edge->toArray();
             $this->assertArrayHasKey('series', $attributes);
             $this->assertEquals($expectedEdgesTypes[$key], $edge->series);
